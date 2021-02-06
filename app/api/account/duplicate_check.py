a975c204c54c3ad95a60f22228b2014d7f4863cc
MUsoftware <musoftware@daum.net>
2021-02-07 02:52:16 +0900

    Make account duplicate check route work

--- app/api/account/duplicate_check.py
+++ app/api/account/duplicate_check.py
@@ -1,18 +1,46 @@
 import flask
 import flask.views
-import app.api as api
+
+import app.common.utils as utils
 import app.database as db_module
-# import app.database.jwt as jwt_module
+import app.database.user as user_module
+
+from app.api.response_case import CommonResponseCase
+from app.api.account.response_case import AccountResponseCase
 
 db = db_module.db
 
 
 class AccountDuplicateCheckRoute(flask.views.MethodView):
     def post(self):
-        # TODO: Duplicate check about post request data
-        return api.create_response(
-            code=200, success=True,
-            message='',
-            data={
+        # Check duplicates about posted request data
+        dupcheck_req = utils.request_body(
+                            required_fields=[],
+                            optional_fields=['email', 'id', 'nickname'])
+        if type(dupcheck_req) == list:
+            return CommonResponseCase.body_required_omitted.create_response(data={'lacks': dupcheck_req})
+        elif dupcheck_req is None:
+            return CommonResponseCase.body_invalid.create_response()
+        elif not dupcheck_req:
+            return CommonResponseCase.body_empty.create_response()
+        elif type(dupcheck_req) != dict:
+            return CommonResponseCase.body_invalid.create_response()
 
-            })
+        field_column_map = {
+            'email': user_module.User.email,
+            'id': user_module.User.id,
+            'nickname': user_module.User.nickname,
+        }
+        check_result = list()
+        try:
+            for field_name, field_value in dupcheck_req:
+                if user_module.User.query.filter(field_column_map[field_name] == field_value).first():
+                    check_result.append(field_name)
+
+            if check_result:
+                return AccountResponseCase.user_already_used.create_response(data={
+                    'duplicate': check_result
+                })
+            return AccountResponseCase.user_safe_to_use.create_response()
+        except Exception:
+            return CommonResponseCase.server_error.create_response()

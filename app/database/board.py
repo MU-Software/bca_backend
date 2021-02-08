c9d91add79f6bfb181bcaaa9a3fc1ae6c30f7c93
MUsoftware <musoftware@daum.net>
2021-02-09 02:58:08 +0900

    Add Tag, Post-Tag relation, Like for Post, Favorite for Post tables

--- app/database/board.py
+++ app/database/board.py
@@ -59,6 +59,9 @@ class Post(db_module.DefaultModelMixin, db.Model):
     modifiable = db.Column(db.Boolean, default=True, nullable=False)
 
     comments: list['Comment'] = None  # Placeholder for backref
+    favorited_by: list['PostFavorite'] = None  # Placeholder for backref
+    liked_by: list['PostLike'] = None  # Placeholder for backref
+    tags: list['PostTagRelation'] = None  # Placeholder for backref
 
 
 class Comment(db_module.DefaultModelMixin, db.Model):
@@ -71,7 +74,10 @@ class Comment(db_module.DefaultModelMixin, db.Model):
     user = db.relationship('User', backref=db.backref('comments', order_by='Comment.modified_at.desc()'))
 
     post_id = db.Column(db_module.PrimaryKeyType, db.ForeignKey('TB_POST.uuid'))
-    post = db.relationship('Post', backref=db.backref('comments', order_by='Comment.created_at.desc()'))
+    post: 'Post' = db.relationship('Post',
+                                   backref=db.backref(
+                                        'comments',
+                                        order_by='Comment.created_at.desc()'))
 
     body = db.Column(db.String, unique=False, nullable=False)
 
@@ -81,3 +87,85 @@ class Comment(db_module.DefaultModelMixin, db.Model):
     readable = db.Column(db.Boolean, default=True, nullable=False)
     modifiable = db.Column(db.Boolean, default=True, nullable=False)
     deletable = db.Column(db.Boolean, default=True, nullable=False)
+
+
+class Tag(db_module.DefaultModelMixin, db.Model):
+    __tablename__ = 'TB_TAG'
+    uuid = db.Column(db_module.PrimaryKeyType,
+                     db.Sequence('SQ_Tag_UUID'),
+                     primary_key=True,
+                     nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+
+class PostTagRelation(db.Model):
+    __tablename__ = 'TB_POST_TAG_RELATION'
+    uuid = db.Column(db_module.PrimaryKeyType,
+                     db.Sequence('SQ_PostTagRelation_UUID'),
+                     primary_key=True,
+                     nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+    post_id = db.Column(db_module.PrimaryKeyType,
+                        db.ForeignKey('TB_POST.uuid'),
+                        nullable=False)
+    post: 'Post' = db.relationship('Post',
+                                   backref=db.backref(
+                                        'tags',
+                                        order_by='PostTagRelation.created_at.desc()'))
+
+    tag_id = db.Column(db_module.PrimaryKeyType,
+                       db.ForeignKey('TB_TAG.uuid'),
+                       nullable=False)
+    tag: 'Tag' = db.relationship('Tag',
+                                 backref=db.backref(
+                                    'posts',
+                                    order_by='PostTagRelation.created_at.desc()'))
+
+
+class PostLike(db_module.DefaultModelMixin, db.Model):
+    __tablename__ = 'TB_POST_LIKE'
+    uuid = db.Column(db_module.PrimaryKeyType,
+                     db.Sequence('SQ_PostLike_UUID'),
+                     primary_key=True,
+                     nullable=False)
+
+    post_id = db.Column(db_module.PrimaryKeyType,
+                        db.ForeignKey('TB_POST.uuid'),
+                        nullable=False)
+    post: 'Post' = db.relationship('Post',
+                                   backref=db.backref(
+                                        'liked_by',
+                                        order_by='PostLike.created_at.desc()'))
+
+    user_id = db.Column(db_module.PrimaryKeyType,
+                        db.ForeignKey('TB_USER.uuid'),
+                        nullable=False)
+    user: 'user_module.User' = db.relationship('User',
+                                               backref=db.backref(
+                                                    'liked_on',
+                                                    order_by='PostLike.created_at.desc()'))
+
+
+class PostFavorite(db_module.DefaultModelMixin, db.Model):
+    __tablename__ = 'TB_POST_FAVORITE'
+    uuid = db.Column(db_module.PrimaryKeyType,
+                     db.Sequence('SQ_PostFavorite_UUID'),
+                     primary_key=True,
+                     nullable=False)
+
+    post_id = db.Column(db_module.PrimaryKeyType,
+                        db.ForeignKey('TB_POST.uuid'),
+                        nullable=False)
+    post: 'Post' = db.relationship('Post',
+                                   backref=db.backref(
+                                        'favorited_by',
+                                        order_by='PostFavorite.created_at.desc()'))
+
+    user_id = db.Column(db_module.PrimaryKeyType,
+                        db.ForeignKey('TB_USER.uuid'),
+                        nullable=False)
+    user: 'user_module.User' = db.relationship('User',
+                                               backref=db.backref(
+                                                    'favorited_on',
+                                                    order_by='PostFavorite.created_at.desc()'))

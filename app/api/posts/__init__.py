35a91e6e792b9bf848a0c0ba8faac6e576e20cfe
MUsoftware <musoftware@daum.net>
2021-02-09 02:58:59 +0900

    Add post route

--- app/api/posts/__init__.py
+++ app/api/posts/__init__.py
@@ -1,2 +1,7 @@
+import app.api.posts.post_paging as post_paging
+import app.api.posts.posts as posts
 
-resource_route = {}  # noqa
+resource_route = {
+    '/posts/': post_paging.PostListRoute,
+    '/posts/<string:post_id>': posts.PostRoute,
+}

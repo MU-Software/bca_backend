6943440539571339b172db9988754a3b433afb24
MUsoftware <musoftware@daum.net>
2021-03-12 22:14:42 +0900

    Fix error to response correctly

--- app/api/request_handler.py
+++ app/api/request_handler.py
@@ -13,7 +13,7 @@ def before_first_request():
 def before_request():
     if flask.current_app.config.get('RESTAPI_VERSION') == 'dev':
         if flask.request.headers.get('X-Development-Key') != flask.current_app.config.get('DEVELOPMENT_KEY'):
-            return AccountResponseCase.user_signed_out.create_response
+            return AccountResponseCase.user_signed_out.create_response()
 
 
 def after_request(response):

c613f1e508a86c9d0c6af2e2644e6d91a80bb99d
MUsoftware <musoftware@daum.net>
2021-04-16 00:12:26 +0900

    Apply sub-path on debug routes

--- app/api/debug/__init__.py
+++ app/api/debug/__init__.py
@@ -1,8 +1,11 @@
 import flask
+import os
+
+restapi_version = os.environ.get('RESTAPI_VERSION')
 
 
 def init_app(app: flask.Flask):
-    @app.route('/debug/<path:path>')
+    @app.route(f'/api/{restapi_version}/debug/<path:path>')
     def static_route(path):
         return flask.send_from_directory('static/debug', path)
 
@@ -10,6 +13,6 @@ def init_app(app: flask.Flask):
 import app.api.debug.decorator_debug as decorator_debug  # noqa
 
 resource_route = {
-    '/debug/decorator/test/<string:debug_id>': decorator_debug.Debug__DecoratorRoute,
-    '/debug/decorator/need-signout': decorator_debug.Debug_UserMustSignOut_DecoratorRoute,
+    f'/api/{restapi_version}/debug/decorator/test/<string:debug_id>': decorator_debug.Debug__DecoratorRoute,
+    f'/api/{restapi_version}/debug/decorator/need-signout': decorator_debug.Debug_UserMustSignOut_DecoratorRoute,
 }

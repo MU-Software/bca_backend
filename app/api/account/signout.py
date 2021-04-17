fe23d330f9a8f6e5f1207889290dfd333181a877
MUsoftware <musoftware@daum.net>
2021-04-18 01:53:13 +0900

    Fix /account/signout route by refactoring

--- app/api/account/signout.py
+++ app/api/account/signout.py
@@ -1,9 +1,9 @@
 import datetime
 import flask
 import flask.views
+import typing
 
 import app.api.helper_class as api_class
-import app.common.utils as utils
 import app.database as db_module
 import app.database.jwt as jwt_module
 
@@ -14,51 +14,30 @@ redis_db = db_module.redis_db
 
 
 class SignOutRoute(flask.views.MethodView, api_class.MethodViewMixin):
+    @api_class.RequestHeader(
+        required_fields={},
+        auth={api_class.AuthType.RefreshToken: False, })
     @api_class.RequestBody(
         required_fields={'signout': {'type': 'string', }, })
-    def post(self, req_body: dict):
+    def post(self, req_body: dict, refresh_token: typing.Optional[jwt_module.RefreshToken] = None):
         '''
         description: Sign-Out and expire user token
         responses:
             - user_signed_out
         '''
-        server_name = flask.current_app.config.get('SERVER_NAME')
-        restapi_version = flask.current_app.config.get('RESTAPI_VERSION')
-
-        refresh_token_remover_cookie = utils.delete_cookie(
-                                            name='refresh_token',
-                                            path=f'/api/{restapi_version}/account',
-                                            domain=server_name if restapi_version != 'dev' else None,
-                                            samesite='None' if restapi_version == 'dev' else 'strict',
-                                            secure=True)
-        refresh_token_remover_header = ('Set-Cookie', refresh_token_remover_cookie)
-
-        refresh_token_cookie = flask.request.cookies.get('refresh_token', None)
-        if refresh_token_cookie:
-            try:
-                refresh_token = jwt_module.RefreshToken.from_token(
-                                    refresh_token_cookie,
-                                    flask.current_app.config.get('SECRET_KEY'))
-            except Exception:
-                return AccountResponseCase.user_signed_out.create_response()
-
+        if refresh_token:
             revoke_target_jti = refresh_token.jti
-            print(f'Refresh token {revoke_target_jti} removed!')
             try:
                 redis_db.set('refresh_revoke=' + str(revoke_target_jti), 'revoked', datetime.timedelta(weeks=2))
+                print(f'Refresh token {revoke_target_jti} registered on REDIS!')
             except Exception:
-                print('Raised error while removing token from REDIS')
+                print('Raised error while registering token from REDIS')
 
             try:
                 db.session.delete(refresh_token)
                 db.session.commit()
+                print(f'Refresh token {revoke_target_jti} removed!')
             except Exception:
                 print('Raised error while removing token from DB')
-
-            return AccountResponseCase.user_signed_out.create_response(
-                header=refresh_token_remover_header,
-                message='Goodbye!')
-
-        return AccountResponseCase.user_signed_out.create_response(
-            header=refresh_token_remover_header,
-            message='User already signed-out')
+            return AccountResponseCase.user_signed_out.create_response(message='Goodbye!')
+        return AccountResponseCase.user_signed_out.create_response(message='User already signed-out')

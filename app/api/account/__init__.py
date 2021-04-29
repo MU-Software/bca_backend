423e9b18070c430d2c1d123c05c6a3423bba6a5d
MUsoftware <musoftware@daum.net>
2021-04-29 10:52:10 +0900

    Remove deprecated API

--- app/api/account/__init__.py
+++ app/api/account/__init__.py
@@ -3,7 +3,6 @@ import app.api.account.signin as signin
 import app.api.account.signout as signout
 import app.api.account.deactivate as deactivate
 import app.api.account.refresh as refresh
-# import app.api.account.token_info as token_info  # DEPRECATED
 import app.api.account.duplicate_check as duplicate_check
 import app.api.account.email_action as email_action
 
@@ -15,5 +14,4 @@ resource_route = {
     '/account/deactivate': deactivate.AccountDeactivationRoute,
     '/account/duplicate': duplicate_check.AccountDuplicateCheckRoute,
     '/account/email/<string:email_token>': email_action.EmailActionRoute,
-    # '/account/tokeninfo': token_info.TokenInfoRoute,  # DEPRECATED
 }

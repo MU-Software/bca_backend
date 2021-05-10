1abba9e1b346b9d69d876eb052d91c434f28da18
MUsoftware <musoftware@daum.net>
2021-05-10 20:35:26 +0900

    Update sync_response_case.py

--- app/api/sync/sync_response_case.py
+++ app/api/sync/sync_response_case.py
@@ -6,11 +6,13 @@ class SyncResponseCase(api_class.ResponseCaseCollector):
         description='Your profile/card db file is outdated, '
                     'so we sent you a new version of db file.',
         code=200, success=True,
-        public_sub_code='sync.ok')
+        public_sub_code='sync.ok',
+        data={'db': ''})
     sync_recreated = api_class.Response(
         description='Your profile/card db file is recreated.',
         code=200, success=True,
-        public_sub_code='sync.recreated')
+        public_sub_code='sync.recreated',
+        data={'db': ''})
 
     sync_latest = api_class.Response(
         description='Your profile/card db file is latest version.',

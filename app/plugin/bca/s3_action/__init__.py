88024ccb82d724f1b949a5cb0ceb5ce62fa31f51
MUsoftware <musoftware@daum.net>
2021-08-24 02:31:25 +0900

    Migrate plugins from previous codebase

---
+++ app/plugin/bca/s3_action/__init__.py
@@ -0,0 +1,216 @@
+import boto3
+import botocore.client
+import flask
+import io
+import pathlib as pt
+import sqlalchemy as sql
+import sqlalchemy.ext.declarative as sqldec
+import sqlalchemy.orm as sqlorm
+import sqlite3
+import tempfile
+import typing
+
+import app.common.utils as utils
+import app.database as db_module
+import app.database.profile as profile_module
+import app.bca.database.user_db_table as bca_user_db_table
+
+
+def create_user_db(user_id: int,
+                   insert_all_data_from_global_db: bool = False,
+                   delete_if_available: bool = False) -> typing.Optional[typing.IO[bytes]]:
+    # bca: create user's db file and upload to s3
+    try:
+        if delete_if_available:
+            delete_user_db(user_id)
+
+        # Create temporary file and connection
+        temp_user_db_file = tempfile.NamedTemporaryFile('w+b', delete=True)
+        temp_user_db_sqlite_conn = sqlite3.connect(temp_user_db_file.name)
+        temp_user_db_engine = sql.create_engine('sqlite://', creator=lambda: temp_user_db_sqlite_conn)
+        temp_user_db_session = sqlorm.scoped_session(
+                                    sqlorm.sessionmaker(
+                                        autocommit=False,
+                                        autoflush=False,
+                                        bind=temp_user_db_engine))
+        temp_user_db_base = sqldec.declarative_base()
+
+        # Create tables
+        ProfileTable = type(
+            'ProfileTable',
+            (temp_user_db_base, bca_user_db_table.Profile),
+            {})
+        CardTable = type(
+            'CardTable',
+            (temp_user_db_base, bca_user_db_table.Card),
+            {})
+        CardSubscriptionTable = type(
+            'CardSubscriptionTable',
+            (temp_user_db_base, bca_user_db_table.CardSubscription),
+            {})
+
+        ProfileTable.__table__.create(temp_user_db_engine)
+        CardTable.__table__.create(temp_user_db_engine)
+        CardSubscriptionTable.__table__.create(temp_user_db_engine)
+
+        if insert_all_data_from_global_db:
+            # Insert data to user db from global db
+            # Find user's profiles, and find all card subscriptions using user's profiles.
+
+            # List of profile's ID that owned by user
+            user_profiles_query = db_module.db.session.query(profile_module.Profile.uuid)\
+                .filter(profile_module.Profile.locked_at == None)\
+                .filter(profile_module.Profile.deleted_at == None)\
+                .filter(profile_module.Profile.user_id == user_id)\
+                .subquery()  # noqa
+
+            # All relations between profiles (owned by user) and cards
+            user_card_relations_query = profile_module.CardSubscription.query\
+                .filter(profile_module.CardSubscription.profile_id.in_(user_profiles_query))
+
+            # All card id that needs to be added to user DB
+            card_ids_that_needs_to_be_added_query = user_card_relations_query\
+                .with_entities(profile_module.CardSubscription.card_id)
+
+            user_subscripting_cards_query = profile_module.Card.query\
+                .filter(
+                    (profile_module.Card.uuid.in_(card_ids_that_needs_to_be_added_query))
+                    | (profile_module.Card.profile_id.in_(user_profiles_query)))\
+                .filter(profile_module.Card.locked_at == None)  # noqa
+
+            # All profile id that needs to be added to user DB
+            profile_ids_that_needs_to_be_added_query = profile_module.Card.query\
+                .filter(profile_module.Card.uuid.in_(card_ids_that_needs_to_be_added_query))\
+                .with_entities(profile_module.Card.profile_id)
+
+            user_following_profiles_query = profile_module.Profile.query\
+                .filter(
+                    (profile_module.Profile.uuid.in_(profile_ids_that_needs_to_be_added_query))
+                    | (profile_module.Profile.user_id == user_id))\
+                .filter(profile_module.Profile.locked_at == None)  # noqa
+
+            user_card_relations: list[profile_module.CardSubscription] = user_card_relations_query.all()
+            user_subscripting_cards: list[profile_module.Card] = user_subscripting_cards_query\
+                .distinct(profile_module.Card.uuid).all()
+            user_following_profiles: list[profile_module.Profile] = user_following_profiles_query\
+                .distinct(profile_module.Profile.uuid).all()
+
+            for profile in user_following_profiles:
+                new_profile = ProfileTable()
+
+                target_columns = (
+                    'uuid', 'description', 'data',
+                    'name', 'email', 'phone', 'sns',
+                    'commit_id', 'created_at', 'modified_at',
+                    'deleted_at', 'why_deleted',
+                    'guestbook', 'announcement', 'private')
+
+                for column in target_columns:
+                    setattr(new_profile, column, getattr(profile, column))
+
+                temp_user_db_session.add(new_profile)
+
+            for card in user_subscripting_cards:
+                new_card = CardTable()
+
+                target_columns = (
+                    'uuid', 'profile_id',
+                    'name', 'data', 'preview_url',
+                    'commit_id', 'created_at', 'modified_at',
+                    'deleted_at', 'why_deleted')
+
+                for column in target_columns:
+                    setattr(new_card, column, getattr(card, column))
+
+                temp_user_db_session.add(new_card)
+
+            for card_relation in user_card_relations:
+                new_card_subscription = CardSubscriptionTable()
+
+                target_columns = (
+                    'uuid', 'profile_id', 'card_id',
+                    'commit_id', 'created_at')
+
+                for column in target_columns:
+                    setattr(new_card_subscription, column, getattr(card_relation, column))
+
+                temp_user_db_session.add(new_card_subscription)
+
+        temp_user_db_session.commit()
+        temp_user_db_engine.dispose()  # Disconnect all connections (for safety)
+
+        temp_user_db_file_pt = pt.Path(temp_user_db_file.name)
+
+        with temp_user_db_file_pt.open('rb') as fp:
+            s3_client = boto3.client('s3', region_name=flask.current_app.config.get('AWS_REGION'))
+            s3_client.upload_fileobj(fp,
+                                     flask.current_app.config.get('AWS_S3_BUCKET_NAME'),
+                                     f'user_db/{user_id}/sync_db.sqlite')
+        return temp_user_db_file
+    except Exception as err:
+        print(utils.get_traceback_msg(err))
+        # TODO: Do something proper while creating and uploading user db file
+        print('Exception raised while creating user sqlite file')
+        return None
+
+
+def get_user_db(user_id: int) -> typing.Union[typing.IO[bytes], io.BytesIO]:
+    bucket_name = flask.current_app.config.get('AWS_S3_BUCKET_NAME')
+    s3 = boto3.client('s3', region_name=flask.current_app.config.get('AWS_REGION'))
+    temp_db_file: io.BytesIO = io.BytesIO()
+    try:
+        s3.download_fileobj(bucket_name, f'user_db/{user_id}/sync_db.sqlite', temp_db_file)
+        temp_db_file.seek(0)
+        return temp_db_file
+    except botocore.client.ClientError as err:
+        if utils.safe_int(err.response['Error']['Code']) == 404:
+            new_db_file = create_user_db(user_id)
+            if new_db_file:
+                return new_db_file
+        raise err
+
+
+def get_user_db_md5(user_id: int) -> str:
+    try:
+        bucket_name = flask.current_app.config.get('AWS_S3_BUCKET_NAME')
+        s3 = boto3.client('s3', region_name=flask.current_app.config.get('AWS_REGION'))
+        return s3.head_object(
+            Bucket=bucket_name,
+            Key=f'user_db/{user_id}/sync_db.sqlite'
+        )['ETag'][1:-1]
+    except botocore.client.ClientError as err:
+        if utils.safe_int(err.response['Error']['Code']) == 404:
+            new_db_file = create_user_db(user_id)
+            if new_db_file:
+                file_md5 = utils.fileobj_md5(new_db_file)
+                new_db_file.close()
+                return file_md5
+        return ''
+    except Exception:
+        return ''
+
+
+def check_user_db_md5(user_id: int, md5hash: str) -> bool:
+    '''
+    This function will check argument "md5hash" to MD5 hash of user's db file on S3.
+    Note that S3's ETag may or may not be an MD5 digest of the object's content data,
+    but we won't use Multipart Upload or Encryption, so it'll be fine,
+    unless AWS changes their rules.
+    See: https://docs.aws.amazon.com/AmazonS3/latest/API/RESTCommonResponseHeaders.html
+    '''
+    return get_user_db_md5(user_id) == md5hash
+
+
+def delete_user_db(user_id: int) -> None:
+    try:
+        bucket_name = flask.current_app.config.get('AWS_S3_BUCKET_NAME')
+        s3 = boto3.client('s3', region_name=flask.current_app.config.get('AWS_REGION'))
+        s3.delete_object(
+            Bucket=bucket_name,
+            Key=f'user_db/{user_id}/sync_db.sqlite')
+    except botocore.client.ClientError as err:
+        if utils.safe_int(err.response['Error']['Code']) == 404:
+            return
+        raise err
+    except Exception as err:
+        raise err
---
+++ app/plugin/bca/sqs_action/__init__.py
@@ -0,0 +1,177 @@
+import boto3
+import dataclasses
+import enum
+import flask
+import flask_sqlalchemy as fsql
+import json
+import sqlalchemy as sql
+import sqlalchemy.ext.declarative as sqldec
+import sqlalchemy.util as sqlutil
+import typing
+
+import app.common.utils as utils
+import app.bca.database.user_db_table as user_db_table
+import app.database.profile as profile_module
+
+
+class UserDBModifyActionCase(enum.Enum):
+    add = enum.auto()
+    modify = enum.auto()
+    delete = enum.auto()
+
+
+@dataclasses.dataclass
+class UserDBModifyData:
+    tablename: str
+    uuid: int
+    action: UserDBModifyActionCase
+    column_data_map: dict[str, typing.Any] = dataclasses.field(default_factory=lambda: {})
+
+
+@dataclasses.dataclass
+class UserDBModifyTaskMessage:
+    db_owner_id: int
+    changes: list[UserDBModifyData]
+
+    def to_dict(self) -> dict:
+        if self.db_owner_id < 0:
+            raise ValueError('db_owner_id must be bigger than 0')
+        if not self.changes:
+            raise ValueError('changes must be given')
+
+        changelog = {
+            'TB_PROFILE': {},
+            'TB_CARD': {},
+            'TB_CARD_SUBSCRIPTION': {},
+        }
+
+        for change_data in self.changes:
+            changelog[change_data.tablename][change_data.uuid] = {
+                'action': change_data.action.name,
+                'data': change_data.column_data_map
+            }
+
+        return {
+            'db_owner_id': self.db_owner_id,
+            'changelog': changelog
+        }
+
+    def add_to_queue(self):
+        sqs_client = boto3.client('sqs')
+        sqs_client.send_message(
+            QueueUrl=flask.current_app.config.get('AWS_TASK_SQS_URL'),
+            MessageBody=json.dumps(self.to_dict(), default=utils.json_default),
+            MessageGroupId='userdbmod1')
+
+
+def create_changelog_from_session(db: fsql.SQLAlchemy) -> list[UserDBModifyData]:
+    target_tables = {
+        'TB_PROFILE': user_db_table.Profile,
+        'TB_CARD': user_db_table.Card,
+        'TB_CARD_SUBSCRIPTION': user_db_table.CardSubscription
+    }
+    changes: list[UserDBModifyData] = list()
+
+    for row_created in db.session.new:
+        if type(row_created).__tablename__ not in target_tables:
+            continue
+
+        user_db_target_table = target_tables[type(row_created).__tablename__]
+
+        user_profile_columns = [col_name for col_name, col in user_db_table.Profile.__dict__.items()
+                                if isinstance(col, sql.Column)]
+        # Need to add declared_attr columns
+        user_profile_columns += [col_name for col_name, col in user_db_table.Profile.__dict__.items()
+                                 if isinstance(col, (sqldec.declared_attr, sqlutil.classproperty))]
+
+        user_card_columns = [col_name for col_name, col in user_db_table.Card.__dict__.items()
+                             if isinstance(col, sql.Column)]
+        # Need to add declared_attr columns
+        user_card_columns += [col_name for col_name, col in user_db_table.Card.__dict__.items()
+                              if isinstance(col, (sqldec.declared_attr, sqlutil.classproperty))]
+
+        user_cardsubsribe_columns = [col_name for col_name, col in user_db_table.CardSubscription.__dict__.items()
+                                     if isinstance(col, sql.Column)]
+        # Need to add declared_attr columns
+        user_cardsubsribe_columns += [col_name for col_name, col in user_db_table.CardSubscription.__dict__.items()
+                                      if isinstance(col, (sqldec.declared_attr, sqlutil.classproperty))]
+
+        if type(row_created).__tablename__ == 'TB_CARD_SUBSCRIBED':
+            row_created: profile_module.CardSubscription = row_created
+            # TODO: Add Card and Profile data too
+            target_card: profile_module.Card = profile_module.Card.query\
+                .filter(profile_module.Card.uuid == row_created.card_id)\
+                .first()
+            target_profile: profile_module.Profile = profile_module.Profile.query\
+                .filter(profile_module.Profile.uuid == target_card.profile_id)\
+                .first()
+            target_profile_data = {name: getattr(target_profile, name) for name in user_profile_columns}
+            target_card_data = {name: getattr(target_card, name) for name in user_card_columns}
+            target_cardsubscribe_data = {name: getattr(row_created, name) for name in user_cardsubsribe_columns}
+            changes += [
+                UserDBModifyData(
+                    tablename=user_db_table.Profile.__tablename__,
+                    uuid=target_profile.uuid,
+                    action=UserDBModifyActionCase.add,
+                    column_data_map=target_profile_data),
+                UserDBModifyData(
+                    tablename=user_db_table.Card.__tablename__,
+                    uuid=target_card.uuid,
+                    action=UserDBModifyActionCase.add,
+                    column_data_map=target_card_data),
+                UserDBModifyData(
+                    tablename=user_db_table.CardSubscription.__tablename__,
+                    uuid=row_created.uuid,
+                    action=UserDBModifyActionCase.add,
+                    column_data_map=target_cardsubscribe_data)]
+        else:
+            # Get target user db column lists to restrict columns
+            target_columns = [col_name for col_name, col in user_db_target_table.__dict__.items()
+                              if isinstance(col, sql.Column)]
+            target_data = {name: getattr(row_created, name) for name in target_columns}
+
+            changes.append(
+                UserDBModifyData(
+                    tablename=user_db_target_table.__tablename__,
+                    uuid=row_created.uuid,
+                    action=UserDBModifyActionCase.add,
+                    column_data_map=target_data))
+
+    for row_modified in db.session.dirty:
+        if type(row_modified).__tablename__ not in target_tables:
+            continue
+
+        user_db_target_table = target_tables[type(row_modified).__tablename__]
+        # Get target user db column lists to restrict columns
+        target_columns = [col_name for col_name, col in user_db_target_table.__dict__.items()
+                          if isinstance(col, sql.Column)]
+
+        # Instances can be considered dirty although there is no changes,
+        # so we need to check it.
+        if row_modified.is_modified():
+            # Get changes and log it to changelog
+            modified_changelog: dict[str, list[typing.Any]] = utils.get_model_changes(row_modified)
+            modified_changelog: dict[str, typing.Any] = {k: v[1] for k, v in modified_changelog.items()
+                                                         if k in target_columns}
+
+            mod_data = UserDBModifyData(
+                tablename=user_db_target_table.__tablename__,
+                uuid=row_modified.uuid,
+                action=UserDBModifyActionCase.modify,
+                column_data_map=modified_changelog)
+            changes.append(mod_data)
+
+    for row_deleted in db.session.deleted:
+        if type(row_deleted).__tablename__ not in target_tables:
+            continue
+
+        user_db_target_table = target_tables[type(row_deleted).__tablename__]
+
+        # TODO: Must delete cards and profiles that are not subscribed to anyone.(NEED_GC)
+        changes.append(
+            UserDBModifyData(
+                tablename=user_db_target_table.__tablename__,
+                uuid=row_modified.uuid,
+                action=UserDBModifyActionCase.delete))
+
+    return changes

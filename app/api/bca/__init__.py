752a62d8c24c28fffd51c4031b6cffaf1012988a
MUsoftware <musoftware@daum.net>
2021-10-02 16:56:03 +0900

    Add missing sync route definition

--- app/api/bca/__init__.py
+++ app/api/bca/__init__.py
@@ -1,8 +1,10 @@
 import app.api.bca.profile as profile_route_collection
 import app.api.bca.card as card_route_collection
 import app.api.bca.chat as chat_route_collection
+import app.api.bca.sync as sync_route_collection
 
 bca_resource_route = dict()
 bca_resource_route.update(profile_route_collection.resource_route)
 bca_resource_route.update(card_route_collection.resource_route)
 bca_resource_route.update(chat_route_collection.resource_route)
+bca_resource_route.update(sync_route_collection.resource_route)

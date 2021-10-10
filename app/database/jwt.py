abd3fcf74d8e5ea390753096bc51e4efc47807a3
MUsoftware <musoftware@daum.net>
2021-10-10 19:17:38 +0900

    Support client fcm token refresh while refreshing tokens

--- app/database/jwt.py
+++ app/database/jwt.py
@@ -372,11 +372,6 @@ def refresh_login_data(refresh_token_jwt: str,
 
     refresh_token = RefreshToken.from_token(refresh_token_jwt, key)
 
-    # Check if client token is same.
-    # client_token must be None if user didn't give any client token while sign in
-    if refresh_token.client_token != client_token:
-        raise jwt.exceptions.InvalidTokenError('Client token mismatch')
-
     # Check device type/OS/browser using User-Agent.
     # We'll refresh token only if it's same with db records
     try:
@@ -426,12 +421,17 @@ def refresh_login_data(refresh_token_jwt: str,
             # We need to send refresh token expiration date anyway
             response_data['refresh_token'] = {'exp': refresh_token.exp, }
 
+    # If client requests token change, then we need to commit this on db
+    elif refresh_token.client_token != client_token:
+        refresh_token.client_token = client_token
+        db.session.commit()
+
     # Now, re-issue Access token
     # Access token can always be re-issued
     access_token = AccessToken.from_refresh_token(refresh_token)
     access_token_jwt = access_token.create_token(key+csrf_token, algorithm, True)
     response_data['access_token'] = {
-        'exp': refresh_token.exp,
+        'exp': access_token.exp,
         'token': access_token_jwt,
     }
 

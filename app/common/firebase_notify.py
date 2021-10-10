fe5d2f11323f28d30f1e5ee9d39e08bb0fa2b2dc
MUsoftware <musoftware@daum.net>
2021-10-10 19:36:12 +0900

    Fix firebase_send_notify working correctly

--- app/common/firebase_notify.py
+++ app/common/firebase_notify.py
@@ -4,41 +4,40 @@ from firebase_admin import messaging
 import flask
 
 
-def firebase_send_notify(title: str, body: str, data: dict = None,
-                         topic: str = 'all', target_token: str = None):
-    cred = credentials.Certificate(flask.current_app.config.get('FIREBASE_CERTIFICATE'))
-    default_app = firebase_admin.initialize_app(cred)  # noqa
+def firebase_send_notify(title: str = None, body: str = None, data: dict = None,
+                         topic: str = None, target_token: str = None):
+    try:
+        cred = credentials.Certificate(flask.current_app.config.get('FIREBASE_CERTIFICATE'))
+        default_app = firebase_admin.initialize_app(cred)  # noqa
+    except ValueError:
+        # default_app is already initialized.
+        pass
 
-    # This registration token comes from the client FCM SDKs.
-    # registration_token = flask.current_app.config.get('FIREBASE_REGISTERATION_TOKEN')
+    if not any((title, body, data)):
+        raise ValueError('At least one of (title, body)|data must be set')
 
-    data = data if data else {}
+    notification = None
+    data = data if data else {'click_action': 'FLUTTER_NOTIFICATION_CLICK', }
+    if any((title, body)):
+        title = title or ''
+        body = body or ''
+        notification = messaging.Notification(title=title, body=body)
 
-    # See documentation on defining a message payload.
     message = messaging.Message(
+        data=data, notification=notification,
+
         # android=messaging.AndroidConfig(
         #     ttl=datetime.timedelta(seconds=3600),
         #     priority='normal',
         # ),
-        android=None,
-        apns=None,
-        webpush=None,
+        # apns=None,
+        # webpush=None,
 
-        data={
-            'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-        },
-        notification=messaging.Notification(title=title, body=body),
-        # notification=None,
-
-        fcm_options=None,
-
-        topic=topic,
         token=target_token,
+        topic=topic,
+        # fcm_options=None,
         # condition=None,
     )
 
-    # Send a message to the device corresponding to the provided
-    # registration token.
     response = messaging.send(message)
-    # Response is a message ID string.
-    print('Successfully sent message:', response)
+    print('Successfully sent message:', response)  # Response is a message ID string.

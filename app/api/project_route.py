c1a50388a0f3139b5c231ec604ac28c164fef628
MU-Software <musoftware@daum.net>
2022-02-25 03:38:00 +0900

    Update B.Ca route definitions

--- app/api/project_route.py
+++ app/api/project_route.py
@@ -1,4 +1,7 @@
 # Import and add project routes here.
 # If you want to make git not to track this file anymore,
 # use `git update-index --skip-worktree app/api/project_route.py`
+import app.api.bca as bca_route
+
 project_resource_routes = dict()
+project_resource_routes.update(bca_route.bca_resource_route)

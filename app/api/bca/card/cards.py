97c349421ff4b75161243290e6b7a77c27e4f05e
MU-Software <musoftware@daum.net>
2022-02-28 00:29:40 +0900

    Fix errors while handling user-uploaded files using internal API

--- app/api/bca/card/cards.py
+++ app/api/bca/card/cards.py
@@ -1,5 +1,6 @@
 import flask
 import flask.views
+import json
 
 import app.common.utils as utils
 import app.api.helper_class as api_class
@@ -27,30 +28,27 @@ class CardMainRoute(flask.views.MethodView, api_class.MethodViewMixin):
             - resource_forbidden
             - server_error
         '''
-        try:
-            target_cards = db.session.query(profile_module.Card)\
-                .filter(profile_module.Card.locked_at.is_(None))\
-                .filter(profile_module.Card.deleted_at.is_(None))\
-                .filter(profile_module.Card.user_id == access_token.user)
+        target_cards = db.session.query(profile_module.Card)\
+            .filter(profile_module.Card.locked_at.is_(None))\
+            .filter(profile_module.Card.deleted_at.is_(None))\
+            .filter(profile_module.Card.user_id == access_token.user)
 
-            if 'X-Profile-Id' in req_header:
-                requested_profile_id = utils.safe_int(req_header.get('X-Profile-Id', 0))
-                if str(requested_profile_id) not in access_token.role:
-                    return ResourceResponseCase.resource_forbidden.create_response(
-                        message='접속하고 계신 프로필은 본인의 프로필이 아닙니다.')
+        if 'X-Profile-Id' in req_header:
+            requested_profile_id = utils.safe_int(req_header.get('X-Profile-Id', 0))
+            if str(requested_profile_id) not in access_token.role:
+                return ResourceResponseCase.resource_forbidden.create_response(
+                    message='접속하고 계신 프로필은 본인의 프로필이 아닙니다.')
 
-                target_cards = target_cards.filter(profile_module.Card.profile_id == requested_profile_id)
+            target_cards = target_cards.filter(profile_module.Card.profile_id == requested_profile_id)
 
-            target_cards = target_cards.all()
-            if not target_cards:
-                response_message = '해당 프로필엔 보유하신 명함이 없습니다.'\
-                                   if 'X-Profile-Id' in req_header else '보유하신 명함이 없습니다.'
-                return ResourceResponseCase.resource_not_found.create_response(message=response_message)
+        target_cards = target_cards.all()
+        if not target_cards:
+            response_message = '해당 프로필엔 보유하신 명함이 없습니다.'\
+                                if 'X-Profile-Id' in req_header else '보유하신 명함이 없습니다.'
+            return ResourceResponseCase.resource_not_found.create_response(message=response_message)
 
-            return ResourceResponseCase.multiple_resources_found.create_response(
-                data={'cards': [card.to_dict() for card in target_cards], })
-        except Exception:
-            return CommonResponseCase.server_error.create_response()
+        return ResourceResponseCase.multiple_resources_found.create_response(
+            data={'cards': [card.to_dict() for card in target_cards], })
 
     @api_class.RequestHeader(
         required_fields={'X-Profile-Id': {'type': 'integer', }, },
@@ -72,49 +70,47 @@ class CardMainRoute(flask.views.MethodView, api_class.MethodViewMixin):
             - body_bad_semantics
             - server_error
         '''
-        try:
-            requested_profile_id = utils.safe_int(req_header['X-Profile-Id'])
-            if str(requested_profile_id) not in access_token.role:
-                return ResourceResponseCase.resource_forbidden.create_response()
+        requested_profile_id = utils.safe_int(req_header['X-Profile-Id'])
+        if str(requested_profile_id) not in access_token.role:
+            return ResourceResponseCase.resource_forbidden.create_response()
 
-            file_upload_enabled: bool = flask.current_app.config.get('FILE_MANAGEMENT_ROUTE_ENABLE', False)
-            if not file_upload_enabled:
-                return CommonResponseCase.http_forbidden.create_response(
-                    message='File upload is not enabled',
-                    data={'reason': 'File upload is not enabled'}, )
+        file_upload_enabled: bool = flask.current_app.config.get('FILE_MANAGEMENT_ROUTE_ENABLE', False)
+        if not file_upload_enabled:
+            return CommonResponseCase.http_forbidden.create_response(
+                message='File upload is not enabled',
+                data={'reason': 'File upload is not enabled'}, )
 
-            target_profile = db.session.query(profile_module.Profile)\
-                .filter(profile_module.Profile.locked_at.is_(None))\
-                .filter(profile_module.Profile.deleted_at.is_(None))\
-                .filter(profile_module.Profile.user_id == access_token.user)\
-                .filter(profile_module.Profile.uuid == requested_profile_id)\
-                .first()
-            if not target_profile:
-                return ResourceResponseCase.resource_not_found.create_response()
+        target_profile = db.session.query(profile_module.Profile)\
+            .filter(profile_module.Profile.locked_at.is_(None))\
+            .filter(profile_module.Profile.deleted_at.is_(None))\
+            .filter(profile_module.Profile.user_id == access_token.user)\
+            .filter(profile_module.Profile.uuid == requested_profile_id)\
+            .first()
+        if not target_profile:
+            return ResourceResponseCase.resource_not_found.create_response()
 
-            # We'll handle upload first to make sure whether upload process success.
-            # This calls internal REST API
-            up_result: api_class.ResponseType = route_filemgr.FileManagementRoute().post()
-            up_res_body, up_res_code, up_res_header = up_result
-            if up_res_code != 201:  # Upload failed
-                return up_result
+        # We'll handle upload first to make sure whether upload process success.
+        # This calls internal REST API
+        up_result: api_class.ResponseType = route_filemgr.FileManagementRoute().post()
+        up_res_body, up_res_code, up_res_header = up_result
+        if up_res_code != 201:  # Upload failed
+            return up_result
+        up_res_body = json.loads(up_res_body.data)
 
-            new_card = profile_module.Card()
-            new_card.user_id = target_profile.user_id
-            new_card.profile_id = target_profile.uuid
-            new_card.name = req_body['name']
-            new_card.data = req_body['data']
-            new_card.preview_url = up_res_body['file']['url']
-            new_card.private = req_body.get('private', True)
+        new_card = profile_module.Card()
+        new_card.user_id = target_profile.user_id
+        new_card.profile_id = target_profile.uuid
+        new_card.name = req_body['name']
+        new_card.data = req_body['data']
+        new_card.preview_url = up_res_body['file']['url']
+        new_card.private = req_body.get('private', True)
 
-            db.session.add(new_card)
+        db.session.add(new_card)
 
-            # Apply changeset on user db
-            with sqs_action.UserDBJournalCreator(db):
-                db.session.commit()
+        # Apply changeset on user db
+        with sqs_action.UserDBJournalCreator(db):
+            db.session.commit()
 
-            return ResourceResponseCase.resource_created.create_response(
-                header=(('ETag', new_card.commit_id, ), ),
-                data={'card': new_card.to_dict(), })
-        except Exception:
-            return CommonResponseCase.server_error.create_response()
+        return ResourceResponseCase.resource_created.create_response(
+            header=(('ETag', new_card.commit_id, ), ),
+            data={'card': new_card.to_dict(), })
--- app/api/bca/profile/profile_img_manage.py
+++ app/api/bca/profile/profile_img_manage.py
@@ -1,5 +1,6 @@
 import flask
 import flask.views
+import json
 
 import app.api.helper_class as api_class
 import app.api.common.file_manage as route_filemgr
@@ -29,50 +30,48 @@ class ProfileImageManagementRoute(flask.views.MethodView, api_class.MethodViewMi
             - resource_prediction_failed
             - server_error
         '''
-        try:
-            target_profile = db.session.query(profile_module.Profile)\
-                .filter(profile_module.Profile.locked_at.is_(None))\
-                .filter(profile_module.Profile.deleted_at.is_(None))\
-                .filter(profile_module.Profile.uuid == profile_id)\
-                .first()
-            if not target_profile:
-                return ResourceResponseCase.resource_not_found.create_response(
-                    message='프로필을 찾을 수 없습니다.')
-            if target_profile.user_id != access_token.user:
-                # Check requested user is the owner of the profile
-                return ResourceResponseCase.resource_forbidden.create_response(
-                    message='프로필을 제작한 사람만이 프로필을 수정할 수 있습니다..')
+        target_profile = db.session.query(profile_module.Profile)\
+            .filter(profile_module.Profile.locked_at.is_(None))\
+            .filter(profile_module.Profile.deleted_at.is_(None))\
+            .filter(profile_module.Profile.uuid == profile_id)\
+            .first()
+        if not target_profile:
+            return ResourceResponseCase.resource_not_found.create_response(
+                message='프로필을 찾을 수 없습니다.')
+        if target_profile.user_id != access_token.user:
+            # Check requested user is the owner of the profile
+            return ResourceResponseCase.resource_forbidden.create_response(
+                message='프로필을 제작한 사람만이 프로필을 수정할 수 있습니다..')
 
-            # Check Etag
-            if target_profile.commit_id != req_header.get('If-Match', None):
-                return ResourceResponseCase.resource_prediction_failed.create_response(
-                    message='프로필이 다른 기기에서 수정된 것 같습니다.\n동기화를 해 주세요.')
+        # Check Etag
+        if target_profile.commit_id != req_header.get('If-Match', None):
+            return ResourceResponseCase.resource_prediction_failed.create_response(
+                message='프로필이 다른 기기에서 수정된 것 같습니다.\n동기화를 해 주세요.')
 
-            # We'll handle upload first to make sure whether upload process success.
-            # We can't revert this when error raised if we delete files first.
+        # We'll handle upload first to make sure whether upload process success.
+        # We can't revert this when error raised if we delete files first.
+        # This calls internal REST API
+        up_result: api_class.ResponseType = route_filemgr.FileManagementRoute().post()
+        up_res_body, up_res_code, up_res_header = up_result
+        if up_res_code != 201:  # Upload failed
+            return up_result
+        up_res_body = json.loads(up_res_body.data)
+
+        if target_profile.image_url:
+            target_file: str = target_profile.image_url.replace('/uploads/', '').replace('/', '').strip()
             # This calls internal REST API
-            up_result: api_class.ResponseType = route_filemgr.FileManagementRoute().post()
-            up_res_body, up_res_code, up_res_header = up_result
-            if up_res_code != 201:  # Upload failed
-                return up_result
+            del_result: api_class.ResponseType = route_filemgr.FileManagementRoute().delete(filename=target_file)
+            del_res_body, del_res_code, del_res_header = del_result
+            if del_res_code != 204:  # Deletion didn't done completely.
+                return CommonResponseCase.server_error.create_response()
 
-            if target_profile.image_url:
-                target_file: str = target_profile.image_url.replace('/uploads/', '').replace('/', '').strip()
-                # This calls internal REST API
-                del_result: api_class.ResponseType = route_filemgr.FileManagementRoute().delete(target_file)
-                del_res_body, del_res_code, del_res_header = del_result
-                if del_res_code != 204:  # Deletion didn't done completely.
-                    return CommonResponseCase.server_error.create_response()
+        target_profile.image_url = up_res_body['data']['file']['url']
 
-            target_profile.image_url = up_res_body['file']['url']
+        # Apply changeset on user db
+        with sqs_action.UserDBJournalCreator(db):
+            db.session.commit()
 
-            # Apply changeset on user db
-            with sqs_action.UserDBJournalCreator(db):
-                db.session.commit()
-
-            return ResourceResponseCase.resource_modified.create_response()
-        except Exception:
-            return CommonResponseCase.server_error.create_response()
+        return ResourceResponseCase.resource_modified.create_response()
 
     @api_class.RequestHeader(
         required_fields={'If-Match': {'type': 'string', }, },
@@ -108,7 +107,7 @@ class ProfileImageManagementRoute(flask.views.MethodView, api_class.MethodViewMi
 
             target_file: str = target_profile.image_url.replace('/uploads/', '').replace('/', '').strip()
             # Internal REST API call
-            del_result: api_class.ResponseType = route_filemgr.FileManagementRoute().delete(target_file)
+            del_result: api_class.ResponseType = route_filemgr.FileManagementRoute().delete(filename=target_file)
             del_res_body, del_res_code, del_res_header = del_result
             if del_res_code != 204:
                 # Deletion didn't done completely.

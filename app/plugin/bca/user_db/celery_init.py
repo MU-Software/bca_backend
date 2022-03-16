f8f17d67e9bf4545dce3955694fd68d523880c60
MU-Software <musoftware@daum.net>
2022-03-16 18:20:40 +0900

    Add Celery worker initializer
    ...This will replace bca_backend_taskmgr repository.

--- app/plugin/bca/user_db/celery_init.py
+++ app/plugin/bca/user_db/celery_init.py
@@ -1,36 +1,30 @@
 import celery
-import flask
-import typing
+import os
 
-celery_app: celery.Celery = None
-CeleryTask: typing.Type[celery.Task] = None
+internal_celery_app: celery.Celery = None
 
 
 def init_celery_app():
-    global celery_app
-    global CeleryTask
+    global internal_celery_app
 
-    if celery_app is None:
+    if internal_celery_app is None:
         celery_backend_url = 'redis://'
-        redis_password = flask.current_app.config.get('REDIS_PASSWORD', None)
+        redis_password = os.environ.get('REDIS_PASSWORD', None)
         if redis_password:
             celery_backend_url += f':{redis_password}@'
-        celery_backend_url += flask.current_app.config.get('REDIS_HOST') + ':'
-        celery_backend_url += str(flask.current_app.config.get('REDIS_PORT'))
+        celery_backend_url += os.environ.get('REDIS_HOST') + ':'
+        celery_backend_url += str(os.environ.get('REDIS_PORT'))
         celery_broker_url = celery_backend_url
 
-        celery_app = celery.Celery(
+        internal_celery_app = celery.Celery(
             main='userdbmod1',
             backend=celery_backend_url,
             broker=celery_broker_url)
-        celery_app.conf.update(flask.current_app.config)
-        celery_app.conf.task_ignore_result = True
+        internal_celery_app.conf.task_ignore_result = True
 
-        class ContextTask(celery_app.Task):
-            def __call__(self, *args, **kwargs):
-                with flask.current_app.app_context():
-                    return self.run(*args, **kwargs)
+        import app.plugin.bca.user_db.journal_handler as journal_handler  # noqa
 
-        celery.Task = CeleryTask = ContextTask
+    return internal_celery_app
 
-    return celery_app
+
+init_celery_app()
